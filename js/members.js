a543a478abd6d1666513a5bf0c5b4efc9fca7a3b
Jo Chuang <josephch405@gmail.com>
2018-02-20 00:14:43 -0500

    csv update 2

--- js/members.js
+++ js/members.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
 function processData(data) {
     var data = $.csv.toObjects(data);
     console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        $("#eb1").append(block2html(data[i]));
+    }
+}
+
+function block2html(data) {
+    var imgNamePos = "<img src = \"./img/" + (data.Image ? data.Image : "justT") + ".jpg\" >" +
+        data.Name + "<br>" + data["Eboard position"];
+
+    return "<div class=\"col-xs-6 col-sm-4 col-md-3 eboard_box \">" +
+        imgNamePos + "<div class = \"col-xs-6 col-sm-4 col-md-3 eboard_box_pop\">" +
+        imgNamePos + "<div class = \"descr\">To be added\
+        </div> </div > </div>"
 }
