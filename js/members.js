ee5b01657ecc26db759a86d0632f13d7eb363929
Jo Chuang <josephch405@gmail.com>
2018-02-26 21:12:34 -0500

    update

--- js/members.js
+++ js/members.js
@@ -1,15 +1,11 @@
-$(document).ready(function() {
-    $.get("./csv/eboard.csv").done(function(data) {
-        processData(data);
-    });
-});
+function block2html_e(data) {
+    var imgNamePos = "<img src = \"./img/" + (data.Image ? data.Image : "justT") + ".jpg\" >" +
+        data.Name + "<br><div class=\"mem_pos\">" + data["Eboard position"] + "</div>";
 
-function processData(data) {
-    var data = $.csv.toObjects(data);
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-        $("#eb1").append(block2html(data[i]));
-    }
+    return "<div class=\"col-xs-6 col-sm-4 col-md-3 eboard_box \">" +
+        imgNamePos + "<div class = \"col-xs-6 col-sm-4 col-md-3 eboard_box_pop\">" +
+        imgNamePos + "<div class = \"descr\">" + data.Year + "<br>" + data.Major +
+        "</div> </div > </div>"
 }
 
 function block2html(data) {
@@ -18,6 +14,22 @@ function block2html(data) {
 
     return "<div class=\"col-xs-6 col-sm-4 col-md-3 eboard_box \">" +
         imgNamePos + "<div class = \"col-xs-6 col-sm-4 col-md-3 eboard_box_pop\">" +
-        imgNamePos + "<div class = \"descr\">To be added\
-        </div> </div > </div>"
+        imgNamePos + "<div class = \"descr\">" + data.Year + "<br>" + data.Major +
+        "</div> </div > </div>"
+}
+
+$(document).ready(function() {
+    $.get("./csv/eboard.csv").done(function(data) {
+        processData(data, "eb1", block2html_e);
+    });
+    $.get("./csv/members.csv").done(function(data) {
+        processData(data, "mem", block2html);
+    });
+});
+
+function processData(data, id, b2h) {
+    var data = $.csv.toObjects(data);
+    for (var i = 0; i < data.length; i++) {
+        $(id).append(b2h(data[i]));
+    }
 }
