1222c57e9eb35ed93da6b7cabb940429cea2c511
Jo Chuang <josephch405@gmail.com>
2018-03-03 17:20:29 -0500

    sheets test

--- js/members.js
+++ js/members.js
@@ -1,34 +1,42 @@
+var f2p = {
+    "Name": 0,
+    "Year": 1,
+    "Major": 2,
+    "Image": 3,
+    "Pos": 4,
+}
+
 function block2html_e(data) {
-    var imgNamePos = "<img src = \"./img/" + (data.Image ? data.Image : "justT") + ".jpg\" >" +
-        data.Name + "<br><div class=\"mem_pos\">" + data["Eboard position"] + "</div>";
+    var imgNamePos = "<img src = \"./img/" + (data[3] ? data[3] : "justT") + ".jpg\" >" +
+        data[0] + "<br><div class=\"mem_pos\">" + data[4] + "</div>";
 
     return "<div class=\"col-xs-6 col-sm-4 col-md-3 eboard_box \">" +
         imgNamePos + "<div class = \"col-xs-6 col-sm-4 col-md-3 eboard_box_pop\">" +
-        imgNamePos + "<div class = \"descr\">" + data.Year + "<br>" + data.Major +
+        imgNamePos + "<div class = \"descr\">" + data[1] + "<br>" + data[2] +
         "</div> </div > </div>"
 }
 
 function block2html(data) {
-    var imgNamePos = "<img src = \"./img/" + (data.Image ? data.Image : "justT") + ".jpg\" >" +
-        data.Name;
+    var imgNamePos = "<img src = \"./img/" + (data[3] ? data[3] : "justT") + ".jpg\" >" +
+        data[0];
 
     return "<div class=\"col-xs-6 col-sm-4 col-md-3 eboard_box \">" +
         imgNamePos + "<div class = \"col-xs-6 col-sm-4 col-md-3 eboard_box_pop\">" +
-        imgNamePos + "<div class = \"descr\">" + data.Year + "<br>" + data.Major +
+        imgNamePos + "<div class = \"descr\">" + data[1] + "<br>" + data[2] +
         "</div> </div > </div>"
 }
 
 $(document).ready(function() {
-    $.get("./csv/eboard.csv").done(function(data) {
+    $.get("https://sheets.googleapis.com/v4/spreadsheets/1i1bcmeKGIjHaWqP57CoD1lj8b2eHLQPIx0MSmUvvhbc/values/eboard!A2:E?key=AIzaSyCMs99x075GStFbTCYNIVlp_yEzvrl6Rl0").done(function(data) {
         processData(data, "#eb1", block2html_e);
     });
-    $.get("./csv/members.csv").done(function(data) {
+    $.get("https://sheets.googleapis.com/v4/spreadsheets/1i1bcmeKGIjHaWqP57CoD1lj8b2eHLQPIx0MSmUvvhbc/values/members!A2:D?key=AIzaSyCMs99x075GStFbTCYNIVlp_yEzvrl6Rl0").done(function(data) {
         processData(data, "#mem", block2html);
     });
 });
 
 function processData(data, id, b2h) {
-    var data = $.csv.toObjects(data);
+    data = data.values;
     for (var i = 0; i < data.length; i++) {
         $(id).append(b2h(data[i]));
     }
